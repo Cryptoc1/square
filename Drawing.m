47c7385b45dd8ea6930f35df8de3e0adca5ac229
Samuel Steele <steelesam72@gmail.com>
2015-04-17 16:39:48 -0400

    Added click listener and cleaned up drawRect

--- Drawing.m
+++ Drawing.m
@@ -7,6 +7,7 @@
 //
 
 #import "Drawing.h"
+#import "AppKit/AppKit.h"
 
 @implementation Drawing
 
@@ -18,65 +19,21 @@
     return self;
 }
 
+- (void)mouseDown:(NSEvent *)theEvent {
+    NSLog(@"Mouse Clicked");
+}
+
 - (void)drawRect:(NSRect)rect
 {
-    // Default stuff
-    // setup basics
-    /*[[NSColor colorWithCalibratedRed:0 green:0 blue:0 alpha:0] set];
-    NSRectFill ( [self bounds] );
-    int count = 12;
-    NSRect startingRect = NSMakeRect ( 100, 100, 100, 100 );
-    
-    // create arrays of rects and colors
-    NSRect    rectArray [count];
-    NSColor * colorArray[count];
-    rectArray [0] = startingRect;
-    colorArray[0] = [NSColor redColor];
-    
-    // populate arrays
-    int i;
-    NSRect oneRect = rectArray[0];
-    for ( i = 1; i < count; i++ )
-    {
-        // advance to the right
-        oneRect.origin.x += 100;
-        
-        // if the right edge doesn't fit, move to next row
-        if ( NSMaxX (oneRect) > NSMaxX ( [self bounds] ) )
-        {
-            oneRect.origin.x = startingRect.origin.x;
-            oneRect.origin.y += 100;
-        }
-        
-        rectArray [i] = oneRect;
-        
-        // increment color
-        colorArray[i] = [NSColor colorWithCalibratedHue: (i*0.04)
-                                             saturation: 1
-                                             brightness: 0.9
-                                                  alpha: 1];
-    }
-    
-    // use rect and color arrays to fill
-    NSRectFillListWithColors ( rectArray, colorArray, count );
-    
-    // draw a border around each rect
-    [[NSColor whiteColor] set];
-    for ( i = 0; i < count; i++) {
-        NSFrameRectWithWidth ( rectArray[i], 2 );
-    }*/
     CGContextRef myContext = [[NSGraphicsContext currentContext] graphicsPort];
-    // ********** Your drawing code here **********
-    CGContextSetRGBFillColor (myContext, 1, 0, 0, 1);
-    CGContextFillRect (myContext, CGRectMake (0, 0, 200, 100 ));
-    CGContextSetRGBFillColor (myContext, 0, 0, 1, .5);
-    CGContextFillRect (myContext, CGRectMake (0, 0, 100, 200));
+    CGContextSetRGBFillColor(myContext, .5, 0, 1, .65);
+    CGContextFillRect(myContext, CGRectMake(200, 200, 100, 100));
     
 }
 
 - (BOOL)isFlipped
 {
-    return NO;
+    return YES;
 }
 
 @end
